07831fab5e5180d54492029ea0d01ad58b8f9558
sriramamadusuthanan <srirama1999sri@gmail.com>
2024-10-28 23:42:07 +0530

    28/10/24

--- src/LinkedList/basics.java
+++ src/LinkedList/basics.java
@@ -7,7 +7,15 @@ public class basics {
 
     public static void main(String[] arg)
     {
+        LinkedList<Integer> list =new LinkedList<>();
 
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.addFirst(0);
+        list.addLast(4);
+
+        reverseLinkedList(list);
     }
 
     //single linledList
@@ -15,7 +23,34 @@ public class basics {
     {
         LinkedList<String> list =new LinkedList<>();
 
+        list.add("sri");
+        list.add("ram");
+        list.add("m");
+
+        list.addFirst("name");
+        list.addLast("end");
+
+        //list.getFirst();
+        //list.getLast();
+
+//        for(String n: list)
+//        {
+//            System.out.println(n);
+//        }
 
 
+    }
+
+    //head = [1,2,3,4,5] reverse using LinkedList
+
+    static void reverseLinkedList(LinkedList<Integer> list)
+    {
+        var reverselist= new LinkedList<Integer>();
+
+        for(int i=list.getLast();i>list.getFirst();i--)
+        {
+           System.out.println(list.get(i));
+           reverselist.add(list.get(i));
+        }
     }
 }
